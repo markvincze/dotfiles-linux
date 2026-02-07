d3e244d54ec3769c0bf6adca735180a30af61b71
Mark Vincze <mvincze@travix.com>
2026-02-07 20:32:10 +0100

    Update config

--- uploadDotfiles.sh
+++ uploadDotfiles.sh
@@ -1,1 +1,14 @@
-a
+echo "Creating folders"
+mkdir -p .config/ghostty
+mkdir -p .config/sway
+mkdir -p .config/nvim
+
+echo "Copying configs"
+cp -f -r ~/.config/ghostty ./.config/
+cp -f -r ~/.config/sway ./.config/
+cp -f -r ~/.config/nvim ./.config/
+cp ~/.zshrc 
+
+git add .
+git commit -m "Update config"
+git push origin main

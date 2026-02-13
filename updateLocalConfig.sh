24d55480ab45705fc8b84b1f68cc9901129455bb
Mark Vincze <hello@markvincze.com>
2026-02-13 22:05:27 +0100

    Copy the waybar config

--- updateLocalConfig.sh
+++ updateLocalConfig.sh
@@ -1,11 +1,13 @@
 echo "Removing config folders"
 rm -rf ~/.config/ghostty
 rm -rf ~/.config/sway
+rm -rf ~/.config/waybar
 rm -rf ~/.config/nvim
 
 echo "Copying config files"
 cp -f -r .config/ghostty ~/.config/
 cp -f -r .config/sway ~/.config/
+cp -f -r .config/waybar ~/.config/
 cp -f -r .config/nvim ~/.config/
 cp -f .zshrc ~/
 

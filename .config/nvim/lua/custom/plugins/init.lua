3a43459074eed3df22f095f3add07edfb7083bdb
Mark Vincze <hello@markvincze.com>
2026-02-13 10:19:15 +0100

    Update config

---
+++ .config/nvim/lua/custom/plugins/init.lua
@@ -0,0 +1,16 @@
+-- You can add your own plugins here or in other files in this directory!
+--  I promise not to create any merge conflicts in this directory :)
+--
+-- See the kickstart.nvim README for more information
+return {
+  {
+    'nvimdev/dashboard-nvim',
+    event = 'VimEnter',
+    config = function()
+      require('dashboard').setup {
+        -- config
+      }
+    end,
+    dependencies = { { 'nvim-tree/nvim-web-devicons' } },
+  },
+}
---
+++ .config/nvim/lua/kickstart/plugins/neo-tree.lua
@@ -0,0 +1,33 @@
+-- Neo-tree is a Neovim plugin to browse the file system
+-- https://github.com/nvim-neo-tree/neo-tree.nvim
+
+return {
+  'nvim-neo-tree/neo-tree.nvim',
+  version = '*',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+    'MunifTanjim/nui.nvim',
+  },
+  lazy = false,
+  keys = {
+    { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
+    { '<C-n>', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
+  },
+  opts = {
+    filesystem = {
+      filtered_items = {
+        visible = true, -- when true, they will just be displayed differently than normal items
+        -- hide_dotfiles = true,
+        -- hide_gitignored = true,
+        -- hide_ignored = true, -- hide files that are ignored by other gitignore-like files
+      },
+      window = {
+        mappings = {
+          ['\\'] = 'close_window',
+          ['/'] = 'none',
+        },
+      },
+    },
+  },
+}

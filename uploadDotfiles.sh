e1dfa2f3ca898b58403536be4bc39db19582c56a
Mark Vincze <hello@markvincze.com>
2026-02-13 21:48:08 +0100

    Update config

--- uploadDotfiles.sh
+++ uploadDotfiles.sh
@@ -1,15 +1,18 @@
 echo "Creating folders"
 rm -rf .config/ghostty
 rm -rf .config/sway
+rm -rf .config/waybar
 rm -rf .config/nvim
 
 mkdir -p .config/ghostty
 mkdir -p .config/sway
+mkdir -p .config/waybar
 mkdir -p .config/nvim
 
 echo "Copying configs"
 cp -f -r ~/.config/ghostty ./.config/
 cp -f -r ~/.config/sway ./.config/
+cp -f -r ~/.config/waybar ./.config/
 cp -f -r ~/.config/nvim ./.config/
 rm -rf ./.config/nvim/.git
 cp ~/.zshrc .
@@ -17,3 +20,4 @@ cp ~/.zshrc .
 git add .
 git commit --allow-empty -m "Update config"
 git push origin main
+

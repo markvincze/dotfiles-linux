fb9c000400b558613bdff2ebc5e29bf9a02483a8
Mark Vincze <hello@markvincze.com>
2026-03-19 14:54:16 +0100

    Update config

--- uploadDotfiles.sh
+++ uploadDotfiles.sh
@@ -3,17 +3,20 @@ rm -rf .config/ghostty
 rm -rf .config/sway
 rm -rf .config/waybar
 rm -rf .config/nvim
+rm -rf .config/noctalia
 
 mkdir -p .config/ghostty
 mkdir -p .config/sway
 mkdir -p .config/waybar
 mkdir -p .config/nvim
+mkdir -p .config/noctalia
 
 echo "Copying configs"
 cp -f -r ~/.config/ghostty ./.config/
 cp -f -r ~/.config/sway ./.config/
 cp -f -r ~/.config/waybar ./.config/
 cp -f -r ~/.config/nvim ./.config/
+cp -f -r ~/.config/noctalia ./.config/
 rm -rf ./.config/nvim/.git
 cp ~/.zshrc .
 

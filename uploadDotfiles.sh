e7d2b8ae880c3715fa453b2cc1c77a1cf6363de2
Mark Vincze <hello@markvincze.com>
2026-09-25 16:25:33 +0200

    Remove waybar

--- uploadDotfiles.sh
+++ uploadDotfiles.sh
@@ -1,20 +1,17 @@
 echo "Creating folders"
 rm -rf .config/ghostty
 rm -rf .config/sway
-rm -rf .config/waybar
 rm -rf .config/nvim
 rm -rf .config/noctalia
 
 mkdir -p .config/ghostty
 mkdir -p .config/sway
-mkdir -p .config/waybar
 mkdir -p .config/nvim
 mkdir -p .config/noctalia
 
 echo "Copying configs"
 cp -f -r ~/.config/ghostty ./.config/
 cp -f -r ~/.config/sway ./.config/
-cp -f -r ~/.config/waybar ./.config/
 cp -f -r ~/.config/nvim ./.config/
 cp -f -r ~/.config/noctalia ./.config/
 rm -rf ./.config/nvim/.git
